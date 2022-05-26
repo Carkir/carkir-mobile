b077b73ef0c6bc3bde0b10f05abf623e8316cc65
Yoga Prasetyo <jobsforyoga@gmail.com>
2022-05-26 22:43:55 +0700

    Create logic and view model on home page

--- app/src/main/java/com/bangkit/capstone/carkirapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/bangkit/capstone/carkirapp/ui/home/HomeViewModel.kt
@@ -1,13 +1,10 @@
 package com.bangkit.capstone.carkirapp.ui.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.bangkit.capstone.carkirapp.data.AppRepository
 
-class HomeViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
+class HomeViewModel(private val repo: AppRepository) : ViewModel() {
+    fun getAllParkingPlaces() = repo.getAllParkingPlace().asLiveData()
+    fun getRecentParkingPlaces() = repo.getRecentParkingPlace().asLiveData()
 }
