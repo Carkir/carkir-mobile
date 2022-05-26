172df578470093f9d8c732fe1790c8f76a9de1d9
Yoga Prasetyo <jobsforyoga@gmail.com>
2022-05-26 21:52:37 +0700

    Add configuration remote source

--- app/src/main/java/com/bangkit/capstone/carkirapp/data/remote/network/ApiService.kt
+++ app/src/main/java/com/bangkit/capstone/carkirapp/data/remote/network/ApiService.kt
@@ -1,5 +1,23 @@
 package com.bangkit.capstone.carkirapp.data.remote.network
 
+import com.bangkit.capstone.carkirapp.data.remote.response.DetailPlaceResponse
+import com.bangkit.capstone.carkirapp.data.remote.response.PlacesResponseItem
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface ApiService {
-    // TODO write code here
+    @GET("read/allPlace")
+    suspend fun getAllPlace(): List<PlacesResponseItem>
+
+    @GET("read/{name}")
+    suspend fun getPlace(
+        @Path("name") name: String
+    ) : DetailPlaceResponse
+
+    // TODO Complete the response type
+    @GET("read/Occupancy/{name}/{floor}")
+    suspend fun getOccupancyFloor(
+        @Path("name") name: String,
+        @Path("floor") floor: Int
+    )
 }
