764134c285f1286ee88fc4d6bdbbd8951cd39e0d
Yoga Prasetyo <jobsforyoga@gmail.com>
2022-05-30 15:00:57 +0700

    Refactoring and handle token on home

--- app/src/main/java/com/bangkit/capstone/carkirapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/bangkit/capstone/carkirapp/ui/home/HomeViewModel.kt
@@ -2,9 +2,18 @@ package com.bangkit.capstone.carkirapp.ui.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.bangkit.capstone.carkirapp.data.AppRepository
+import kotlinx.coroutines.launch
 
 class HomeViewModel(private val repo: AppRepository) : ViewModel() {
-    fun getAllParkingPlaces() = repo.getAllParkingPlace().asLiveData()
+    fun getAllParkingPlaces(token: String) = repo.getAllParkingPlace(token).asLiveData()
     fun getRecentParkingPlaces() = repo.getRecentParkingPlace().asLiveData()
+    fun loadTokenFromDataStore() = repo.getTokenFromDataStore().asLiveData()
+
+    fun requestToken() {
+        viewModelScope.launch {
+            repo.tokenKey()
+        }
+    }
 }
