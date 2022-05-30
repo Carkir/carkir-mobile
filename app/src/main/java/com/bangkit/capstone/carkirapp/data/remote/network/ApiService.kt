5c11d4e7e27c85802fc27ea1000457109f4813cf
Yoga Prasetyo <jobsforyoga@gmail.com>
2022-05-30 14:56:32 +0700

    Add authorization for all url API

--- app/src/main/java/com/bangkit/capstone/carkirapp/data/remote/network/ApiService.kt
+++ app/src/main/java/com/bangkit/capstone/carkirapp/data/remote/network/ApiService.kt
@@ -3,21 +3,31 @@ package com.bangkit.capstone.carkirapp.data.remote.network
 import com.bangkit.capstone.carkirapp.data.remote.response.DetailPlaceResponse
 import com.bangkit.capstone.carkirapp.data.remote.response.OccupancyPlaceResponseItem
 import com.bangkit.capstone.carkirapp.data.remote.response.PlacesResponseItem
-import retrofit2.http.GET
-import retrofit2.http.Path
+import com.bangkit.capstone.carkirapp.data.remote.response.TokenResponse
+import com.bangkit.capstone.carkirapp.model.AndroidModel
+import retrofit2.http.*
 
 interface ApiService {
+    @POST(" tokenizable")
+    suspend fun getToken(
+        @Body android: AndroidModel
+    ) : TokenResponse
+
     @GET("read/allPlace")
-    suspend fun getAllPlace(): List<PlacesResponseItem>
+    suspend fun getAllPlace(
+        @Header("Authorization") authorization: String
+    ): List<PlacesResponseItem>
 
     @GET("read/{name}")
     suspend fun getPlace(
+        @Header("Authorization") authorization: String,
         @Path("name") name: String
-    ) : DetailPlaceResponse
+    ): DetailPlaceResponse
 
     @GET("read/Occupancy/{name}/{floor}")
     suspend fun getOccupancyFloor(
+        @Header("Authorization") authorization: String,
         @Path("name") name: String,
         @Path("floor") floor: Int
-    ) : List<OccupancyPlaceResponseItem>
+    ): List<OccupancyPlaceResponseItem>
 }
