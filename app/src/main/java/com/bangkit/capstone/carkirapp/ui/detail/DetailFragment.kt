e37385c1bc7b1c0bcf637d25addb93a252d79bbd
Yoga Prasetyo <jobsforyoga@gmail.com>
2022-05-30 15:04:22 +0700

    Handle token, add datastore, and build floor logic on detail

--- app/src/main/java/com/bangkit/capstone/carkirapp/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/bangkit/capstone/carkirapp/ui/detail/DetailFragment.kt
@@ -1,19 +1,35 @@
 package com.bangkit.capstone.carkirapp.ui.detail
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.bangkit.capstone.carkirapp.R
 import com.bangkit.capstone.carkirapp.data.Resource
+import com.bangkit.capstone.carkirapp.data.local.entity.HistoryEntity
 import com.bangkit.capstone.carkirapp.data.remote.response.DetailPlaceResponse
 import com.bangkit.capstone.carkirapp.databinding.FragmentDetailBinding
+import com.bangkit.capstone.carkirapp.model.FloorAndClusterModel
 import com.bangkit.capstone.carkirapp.model.ViewModelFactory
+import com.bangkit.capstone.carkirapp.ui.adapter.FloorAndClusterAdapter
+import com.bangkit.capstone.carkirapp.utils.decodeBase64ToBitmap
+import com.bangkit.capstone.carkirapp.utils.loadImage
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class DetailFragment : Fragment() {
+
+    // Declaration for adapter
+    private lateinit var floorAdapter: FloorAndClusterAdapter
 
     // Declaration for binding view
     private var _binding: FragmentDetailBinding? = null
@@ -21,7 +37,10 @@ class DetailFragment : Fragment() {
 
     // View Model initialization using delegate by viewModels
     private val detailViewModel: DetailViewModel by viewModels {
-        ViewModelFactory.getInstance(requireContext())
+        ViewModelFactory.getInstance(
+            requireContext(),
+            requireContext().dataStore
+        )
     }
 
     // Receive args from other Fragments to get the name of parking place
@@ -33,6 +52,7 @@ class DetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        detailViewModel.requestToken() // request token before call private API
         return binding.root
     }
 
@@ -40,22 +60,34 @@ class DetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val namePlace = args.namePlace
+        floorAdapter = FloorAndClusterAdapter()
 
-        // Load data from the server to get detail data parking place
-        // Response will be checking, if not null, data will be execute it based on the state
-        detailViewModel.loadDetailParkingPlace(namePlace).observe(viewLifecycleOwner) { response ->
-            if (response != null) {
-                when (response) {
-                    is Resource.Loading -> {} // TODO HANDLE LOADING STATE
-                    is Resource.Success -> showDetailPlace(response.data)
-                    is Resource.Error -> {} // TODO HANDLE ERROR STATE
+        // Load token from local data store
+        // This token for call private API in the server
+        detailViewModel.loadTokenFromDataStore().observe(viewLifecycleOwner) { token ->
+
+            // Load data from the server to get detail data parking place
+            // Response will be checking, if not null, data will be execute it based on the state
+            detailViewModel.loadDetailParkingPlace(token, namePlace)
+                .observe(viewLifecycleOwner) { response ->
+                    if (response != null) {
+                        when (response) {
+                            is Resource.Loading -> {} // TODO HANDLE LOADING STATE
+                            is Resource.Success -> showDetailPlace(response.data)
+                            is Resource.Error -> {
+                                Log.d("DetailFragment", response.message.toString())
+                            } // TODO HANDLE ERROR STATE
+                        }
+                    }
                 }
-            }
         }
 
         binding.fabFavoriteLocation.setOnClickListener {
             // TODO ADD OR REMOVE FAVORITE PARKING PLACE
         }
+
+        // Set adapter to recycler view
+        binding.rvClustersParking.adapter = floorAdapter
     }
 
     /**
@@ -63,16 +95,90 @@ class DetailFragment : Fragment() {
      * The data response will be binding to the layout.
      * */
     private fun showDetailPlace(data: DetailPlaceResponse?) {
-        val (priceLow, name, time, totalEmptySpace, priceHigh, status) = data as DetailPlaceResponse
+        if (data == null) return
+
+        val (priceLow, name, location, time, totalEmptySpace, priceHigh, status, alamat, image) = data
         binding.apply {
+            // TODO UNCOMMENT THE CODE IF IMAGE IS FIXED
+            // val bitmap = image.decodeBase64ToBitmap()
+            // ivPlaceDetail.loadImage(requireContext(), bitmap)
             tvStatusPlaceDetail.text = status
             tvTimePlaceDetail.text = time
             tvNamePlaceDetail.text = name
+            tvAddressPlaceDetail.text = alamat
             tvPriceFirstHour.text = getString(R.string.carkir_detail_price_low, priceLow)
-            tvPriceNextHour.text = getString(R.string.carkir_detail_price_low, priceHigh)
+            tvPriceNextHour.text = getString(R.string.carkir_detail_price_high, priceHigh)
             tvTotalEmptySpacePlaceDetail.text =
                 getString(R.string.carkir_detail_title_space, totalEmptySpace)
         }
+
+        // Add history parking place
+        detailViewModel.addPlaceToHistory(
+            HistoryEntity(
+                name = name,
+                totalSpace = totalEmptySpace,
+                time = time
+            )
+        )
+
+        showFloorAndClusters(name, location)
+    }
+
+    /**
+     * Set data to the adapter recycler view
+     * if data is empty just show info message
+     * */
+    private fun showFloorAndClusters(namePlace: String, floorClusters: List<String>) {
+        if (floorClusters.isNullOrEmpty()) {
+            binding.tvEmptyClustersParking.isVisible = true
+        } else {
+            binding.tvEmptyClustersParking.isVisible = false
+            floorAdapter.submitList(modifiedFloorClusters(namePlace, floorClusters))
+        }
+    }
+
+    // TODO DOCUMENTATION
+    private fun modifiedFloorClusters(
+        name: String,
+        cluster: List<String>
+    ): List<FloorAndClusterModel> {
+        val floorAndClusters = ArrayList<FloorAndClusterModel>()
+        var floor = '1'
+        val rangeClusters = StringBuilder()
+        var totalParkingSpace = 0
+
+        // Add all floor and cluster, except the last floor and clusters
+        for (i in 1 until cluster.size) {
+            if (floor == cluster[i][0]) {
+                rangeClusters.append(cluster[i][1])
+                totalParkingSpace += (cluster[i][2]).toString().toInt()
+            } else {
+                val data = FloorAndClusterModel(
+                    namePlace = name,
+                    floor = floor.toString().toInt(),
+                    rangeClusters = "$floor${rangeClusters.first()} - $floor${rangeClusters.last()}",
+                    parkingSpace = totalParkingSpace
+                )
+                floorAndClusters.add(data)
+
+                rangeClusters.clear()
+                totalParkingSpace = 0
+                floor = cluster[i][0]
+                rangeClusters.append(cluster[i][1])
+                totalParkingSpace += (cluster[i][2]).toString().toInt()
+            }
+        }
+
+        // Add last floor and range clusters
+        val lastData = FloorAndClusterModel(
+            namePlace = name,
+            floor = floor.toString().toInt(),
+            rangeClusters = "$floor${rangeClusters.first()} - $floor${rangeClusters.last()}",
+            parkingSpace = totalParkingSpace
+        )
+        floorAndClusters.add(lastData)
+
+        return floorAndClusters
     }
 
     /**
--- app/src/main/java/com/bangkit/capstone/carkirapp/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/bangkit/capstone/carkirapp/ui/detail/DetailViewModel.kt
@@ -5,11 +5,26 @@ import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.bangkit.capstone.carkirapp.data.AppRepository
 import com.bangkit.capstone.carkirapp.data.local.entity.FavoriteEntity
+import com.bangkit.capstone.carkirapp.data.local.entity.HistoryEntity
 import kotlinx.coroutines.launch
 
 class DetailViewModel(private val repo: AppRepository) : ViewModel() {
 
-    fun loadDetailParkingPlace(name: String) = repo.getParkingPlace(name).asLiveData()
+    fun loadDetailParkingPlace(token: String, name: String) = repo.getParkingPlace(token, name).asLiveData()
+    fun loadTokenFromDataStore() = repo.getTokenFromDataStore().asLiveData()
+
+    fun requestToken() {
+        viewModelScope.launch {
+            repo.tokenKey()
+        }
+    }
+
+    // Operation to add parking place to history local database
+    fun addPlaceToHistory(place: HistoryEntity) {
+        viewModelScope.launch {
+            repo.addPlaceToHistory(place)
+        }
+    }
 
     // Operation to add parking place to local database
     fun addPlaceToFavorite(place: FavoriteEntity) {
